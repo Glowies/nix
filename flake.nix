60e96cdec9d6dfe384f1281d4f2ae0c2e74d4222
glowies <comuokta@gmail.com>
2025-03-02 00:44:28 -0500

    feat: enable tmux

--- flake.nix
+++ flake.nix
@@ -46,9 +46,6 @@
           # Necessary for using flakes on this system.
           nix.settings.experimental-features = "nix-command flakes";
 
-          # Enable alternative shell support in nix-darwin.
-          # programs.fish.enable = true;
-
           # Set Git commit hash for darwin-version.
           system.configurationRevision = self.rev or self.dirtyRev or null;
 
@@ -135,6 +132,14 @@
               RunAtLoad = true;
               ProcessType = "Interactive";
             };
+          };
+
+          # set up tmux
+          programs.tmux = {
+            enable = true;
+            enableMouse = true;
+            enableFzf = true;
+            enableVim = true;
           };
         };
     in
