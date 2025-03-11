40c0e306e3e7501b8ef37b21800b978bc6a78bfa
glowies <comuokta@gmail.com>
2025-03-10 22:55:48 -0400

    feat: transparent background for catppuccin

--- dotfiles/nvim/lua/plugins/example.lua
+++ dotfiles/nvim/lua/plugins/example.lua
@@ -9,6 +9,16 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
+    priority = 1000,
+    opts = {
+      term_colors = true,
+      transparent_background = true,
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.75, -- percentage of the shade to apply to the inactive window
+      },
+    },
   },
 
   -- Configure LazyVim to load catppuccin
